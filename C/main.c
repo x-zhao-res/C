bc3572b931232f9c7517f51275ccfa2b032a7275
x-zhao-res <1025496505@qq.com>
2021-03-21 01:02:58 +0800

    feat:初始化作业指针作业！

--- C/main.c
+++ C/main.c
@@ -1,8 +1,11 @@
 #include<stdio.h>
 #include"fun1.h"
 int main() {
-	printf("123123");
-	charss();
+	int a;
+	printf("欢迎来到学生成绩统计系统\n");
+	scanf_s("%d", &a);
+	printf("你输入的数值是%d", a);
 	getchar();
 	return 0;
+
 }
